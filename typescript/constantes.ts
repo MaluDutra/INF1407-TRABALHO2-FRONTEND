1b4f01476e0ea90755da9a0ccbb3a096b1437802
Maria Luiza Dutra <132154059+MaluDutra@users.noreply.github.com>
2026-06-26 15:39:07 +0000

    Ajusta endereço do backend

--- typescript/constantes.ts
+++ typescript/constantes.ts
@@ -1,7 +1,8 @@
 // constantes.ts
 
 // export const backendAddress = 'http://127.0.0.1:8000/';
-export const backendAddress = 'https://psychic-space-eureka-px9rj7gx565f74j4-8000.app.github.dev/';
+// export const backendAddress = 'https://psychic-space-eureka-px9rj7gx565f74j4-8000.app.github.dev/';
+export const backendAddress = 'https://inf1407-backend.onrender.com/'
 
 /**
  * Base path para as rotas do site.
